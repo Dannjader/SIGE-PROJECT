12c8c62adacf3a5597060a4ef9f19a7d37c50477
Jader <dannjader@gmail.com>
2023-09-01 16:45:55 -0500

    Revert "Se agrega tailwindcss al proyecto"
    This reverts commit 879020db2931337204a3ebb56c08f6abd2e42df3.

--- SIGE/settings.py
+++ SIGE/settings.py
@@ -40,8 +40,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'dispositivos.apps.DispositivosConfig',
-    'tailwind',
+    'tailwind',   
+    'theme',
+    'django_browser_reload'
 ]
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+NPM_BIN_PATH = r"C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -51,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'SIGE.urls'
