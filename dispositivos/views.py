99284741f145ff44f6de0934f4668b6685318f09
Jader <dannjader@gmail.com>
2023-09-04 10:46:05 -0500

    guardo cambios de esta rama

--- dispositivos/views.py
+++ dispositivos/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from dispositivos.models import Reporte, Dispositivo, Servicio
 
 # # Create your views here.
 
-
+@login_required
 def inicio(request):
     return render(request, 'index.html')
 
@@ -16,10 +17,19 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('dispositivos')
+            return redirect('')
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
+
+
+def vista_de_dispositivos(request):
+    query = request.GET.get('', '')
+    if query:
+        dispositivos = Dispositivo.objects.filter(campo__icontains=query)
+    else:
+        dispositivos = Dispositivo.objects.all()
+    return render(request, 'dispositivos/dispositivos_list.html', {'dispositivos': dispositivos})
 
 
 def dispositivos_list(request):
